c86d796055b51e9a583a50784ca1149610ead766
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-01-25 18:01:01 -0300

    arruma lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
- return species.filter((bichos) => ids.includes(bichos.id));
+  return species.filter((bichos) => ids.includes(bichos.id));
 }
 
 module.exports = getSpeciesByIds;
