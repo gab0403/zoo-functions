b3544855d32d776f1b443417b59755e1e5e0fa96
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-01-27 20:53:01 -0300

    Requisito 10 - realizado

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,45 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+function locations(...ids) {
+  const animalObj = species.filter((animal) => ids.includes(animal.id));
+  const result = animalObj.map((home) => home.location);
+  return result;
+}
+function objAnimal(...ids) {
+  const animalObj = species.filter((animal) => ids.includes(animal.id));
+  const result = animalObj.map((element) => element.name);
+  return result;
+}
+function theEmployees() {
+  const newArray = employees.map((element) => ({
+    id: element.id,
+    fullName: `${element.firstName} ${element.lastName}`,
+    species: objAnimal(...element.responsibleFor),
+    locations: locations(...element.responsibleFor),
+  }));
+  return newArray;
 }
 
+function getEmployeesCoverage(object) {
+  if (!object) {
+    return theEmployees();
+  }
+  const { id, name } = object;
+  const employee = data.employees.find((emp) =>
+    emp.firstName === name || emp.lastName === name || emp.id === id);
+  if (!employee) {
+    throw new Error('Informações inválidas');
+  }
+  const employeesObj = employees.find((element) =>
+    element.id === id || element.firstName === name || element.lastName === name);
+  const { firstName, lastName } = employeesObj;
+  return {
+    id: employeesObj.id,
+    fullName: `${firstName} ${lastName}`,
+    species: objAnimal(...employeesObj.responsibleFor),
+    locations: locations(...employeesObj.responsibleFor),
+  };
+}
 module.exports = getEmployeesCoverage;
