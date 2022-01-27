0072660407c3acf056ef277808ce69e4f5e9232a
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-01-27 11:09:16 -0300

    Requisito 5 e 6 - realizados

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((people) => people.age < 18);
+  const adult = entrants.filter(
+    (people) => people.age >= 18 && people.age < 50,
+  );
+  const senior = entrants.filter((people) => people.age >= 50);
+  const result = {
+    child: child.length,
+    adult: adult.length,
+    senior: senior.length,
+  };
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const classificatedEntrants = countEntrants(entrants);
+  const totalChild = classificatedEntrants.child * data.prices.child;
+  const totalAdult = classificatedEntrants.adult * data.prices.adult;
+  const totalSenior = classificatedEntrants.senior * data.prices.senior;
+  return totalChild + totalAdult + totalSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const totalAnimals = {};
+    data.species.forEach((specie) => {
+      totalAnimals[specie.name] = specie.residents.length;
+    });
+    return totalAnimals;
+  }
+  const { specie, sex } = animal;
+  if (!sex) {
+    const objAnimal = data.species.find((item) => item.name === animal.specie);
+    return objAnimal.residents.length;
+  }
+  const animalList = data.species.find((item) => item.name === specie);
+  const sexList = animalList.residents.filter(
+    (resident) => resident.sex === sex,
+  );
+  return sexList.length;
 }
 
 module.exports = countAnimals;
